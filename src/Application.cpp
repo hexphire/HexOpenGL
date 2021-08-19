032e60e791672fc7d6e2b86d5a932e912415fff9
Hexphire <hexphirebiz@gmail.com>
2021-08-19 00:34:14 -0700

    error handling implemented, index buffer implemented, drawn shap is now a square.

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,27 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "):" << function <<
+            " " << file << " " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderSource
 {
     std::string VertexSource;
@@ -119,20 +140,33 @@ int main(void)
     std::cout << glGetString(GL_VERSION);
 
     //triangle position verticies
-    float positions[6] = 
-    {  
+    float positions[] ={  
        -0.5f, -0.5f,
-        0.0f,  0.5f,
-        0.5f, -0.5f
+        0.5f, -0.5f,
+        0.5f,  0.5f,        
+       -0.5f,  0.5f
+    };
+
+    unsigned int indices[] ={
+        0, 1, 2,
+        2, 3, 0,
     };
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);   //create vertex buffer
-    glBindBuffer(GL_ARRAY_BUFFER, buffer); //make buffer active "bind" it.
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); //place data into buffer, along with some basic size and use case info
+    GLCall(glGenBuffers(1, &buffer));   //create vertex buffer
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); //make buffer active "bind" it.
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW)); //place data into buffer, along with some basic size and use case info
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);   //create element buffer
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo)); //make buffer active "bind" it.
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW)); //place data into buffer, along with some basic size and use case info
+
+
+
+
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
     ShaderSource source = ParseShader("res/shaders/basic.shader");
     
@@ -146,23 +180,24 @@ int main(void)
     
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+       
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        GLCall(glfwSwapBuffers(window));
 
         /* Poll for and process events */
-        glfwPollEvents();
+        GLCall(glfwPollEvents());
     }
 
     glDeleteProgram(shader);
